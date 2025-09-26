67c747f7050a37b6a7ba2d6c0bfdb3ef5c997e3e
DeivisStasuls <deivis.stasuls@gmail.com>
2025-09-26 08:57:20 +0300

    switch-case outside of while cycle

--- library-broken.php
+++ library-broken.php
@@ -30,18 +30,7 @@ function displayBook($id, $book) {
     echo "ID: {$id} // Title: ". $book['title'] . " // Author: " . $book['author']. "\n\n";
 }
 
-
-echo "\n\nWelcome to the Library\n";
-
-do {
-    echo "\n\n";
-    echo "1 - show all books\n";
-    echo "2 - show a book\n";
-    echo "3 - add a book\n";
-    echo "4 - delete a book\n";
-    echo "5 - quit\n\n";
-    $choice = readline();
-
+function choice($choice,&$books){
     switch ($choice) {
         case 1:
             foreach ($books as $id => $book) {
@@ -70,5 +59,18 @@ do {
         default:
             echo "Invalid choice\n";
     };
+}
+echo "\n\nWelcome to the Library\n";
+
+do {
+    echo "\n\n";
+    echo "1 - show all books\n";
+    echo "2 - show a book\n";
+    echo "3 - add a book\n";
+    echo "4 - delete a book\n";
+    echo "5 - quit\n\n";
+    $choice = readline();
+    choice($choice,$books);
+    
 
 } while ($continue == true);
